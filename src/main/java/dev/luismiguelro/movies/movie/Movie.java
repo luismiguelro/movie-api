96ca5147c1db347e9a8e7c15fd8d0d4177b64b2f
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-05 10:23:58 -0500

    [feat] add DB operations

---
+++ src/main/java/dev/luismiguelro/movies/movie/Movie.java
@@ -0,0 +1,32 @@
+package dev.luismiguelro.movies.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies") // represents movies collection
+@Data // gets and setters
+@AllArgsConstructor // constructor for the fields
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // reference
+    private List<Review> reviewIds;
+
+
+
+}

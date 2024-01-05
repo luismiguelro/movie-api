f969bcea2ae156825bad068191e7a1a3efca5fea
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-05 11:46:44 -0500

    [feat] add reviews endpoints

--- src/main/java/dev/luismiguelro/movies/reviews/controller/ReviewController.java
+++ src/main/java/dev/luismiguelro/movies/reviews/controller/ReviewController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;

5b49c376570731cab2ebdf2b057eb07a6fcab436
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-01-18 10:47:35 -0500

    [feat] add GlobalException

--- src/main/java/dev/luismiguelro/movies/MoviesApplication.java
+++ src/main/java/dev/luismiguelro/movies/MoviesApplication.java
@@ -1,13 +1,13 @@
 package dev.luismiguelro.movies;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesApplication {
 	public static void main(String[] args) {
-		// Inicia la aplicación Spring Boot
+		// Punto de interrupción o impresión de información
+		System.out.println("Iniciando la aplicación Spring Boot...");
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-
 }
-

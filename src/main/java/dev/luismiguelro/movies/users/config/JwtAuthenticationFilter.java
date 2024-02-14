f0895ada47880fe00f16cdfd21217d97b3580143
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-02-14 10:17:58 -0500

    [feat] update the SecurityContextHolder and finalise the jwt filter

--- src/main/java/dev/luismiguelro/movies/users/config/JwtAuthenticationFilter.java
+++ src/main/java/dev/luismiguelro/movies/users/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +18,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -34,6 +38,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
 
         // extract user email from jwt toker
-        userEmail = jwtService.extractUserEmail(jwt);
+        userEmail = jwtService.extractUserName(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }

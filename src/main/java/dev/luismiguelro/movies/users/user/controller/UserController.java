0d203fd341a02d43037a3a068b31f91c43364ac2
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-02-29 11:22:39 -0500

    [feat] users: get info user

---
+++ src/main/java/dev/luismiguelro/movies/users/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package dev.luismiguelro.movies.users.user.controller;
+import dev.luismiguelro.movies.users.user.User;
+import dev.luismiguelro.movies.users.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+}
---
+++ src/main/java/dev/luismiguelro/movies/users/user/service/UserService.java
@@ -0,0 +1,25 @@
+package dev.luismiguelro.movies.users.user.service;
+
+import dev.luismiguelro.movies.users.user.User;
+import dev.luismiguelro.movies.users.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}

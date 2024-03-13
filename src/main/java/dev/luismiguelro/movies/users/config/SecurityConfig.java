5c9ffe4366f8c757c07b5fd9e29bde365105ab0b
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2024-03-13 11:00:52 -0500

    [fix] browser back button after logout

--- src/main/java/dev/luismiguelro/movies/users/config/SecurityConfig.java
+++ src/main/java/dev/luismiguelro/movies/users/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package dev.luismiguelro.movies.users.config;
 
 import dev.luismiguelro.movies.users.user.service.UserService;
-import jakarta.servlet.Filter;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +20,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -31,28 +35,58 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/auth/**", "/", "/js/**", "/css/**","/video/**")
+                        .requestMatchers("/api/v1/auth/**", "/", "/js/**", "/css/**", "/video/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
+
                 )
                 .formLogin(login -> login
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/home") // Redirigir a /home después de una autenticación exitosa
                 )
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .logoutSuccessUrl("/login?logout")
+                        .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                         .permitAll()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
                 )
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity
+                .headers(AbstractHttpConfigurer::disable
+                )
+                .addFilterBefore(new NoCacheFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
+    }
+
+    @WebFilter
+    public class NoCacheFilter implements Filter {
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+            Filter.super.init(filterConfig);
+        }
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            HttpServletResponse hsr = (HttpServletResponse) servletResponse;
+            hsr.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+            hsr.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            hsr.setDateHeader("Expires", 0); // Proxies.
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+        @Override
+        public void destroy() {
+            Filter.super.destroy();
+        }
     }
 }
